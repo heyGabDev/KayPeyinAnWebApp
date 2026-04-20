a0d13b4d1ed85a22060fb4be3bbb4420862cece0
heyGabDev <heygab.consulting@gmail.com>
2026-04-20 22:41:14 +0200

    feat(basket): add notification #45

--- kayPeyinAnApp/src/app/app.config.ts
+++ kayPeyinAnApp/src/app/app.config.ts
@@ -1,8 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -11,5 +9,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+
+
   ]
 };
---
+++ kayPeyinAnApp/src/app/core/services/notification.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+
+export type NotificationType = 'success' | 'error' | 'info' | 'warning';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService  {
+  
+  constructor(private snackBar: MatSnackBar) {}
+  
+  private show(message: string, type: NotificationType): void {
+    const config: MatSnackBarConfig = {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: [`snack-${type}`]
+    };
+    //this.snackBar.open(message, '✕', config);
+    const ref = this.snackBar.open(message, '✕', config);
+  }
+  
+  success(message: string): void {
+    this.show(message, 'success');
+  }
+    
+  error(message: string): void {
+    this.show(message, 'error');
+  }
+
+  info(message: string): void {
+    this.show(message, 'info');
+  }
+
+  warning(message: string): void {
+    this.show(message, 'warning');
+  }
+}
